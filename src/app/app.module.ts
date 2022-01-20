69e670ebc9a85b398bb3c1ec9f95c6f7eba2e237
perevill <pevipey@gmail.com>
2022-01-20 10:23:09 +0100

    creacion de componentes crud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { EditComponent } from './usuario/edit/usuario-edit.component';
+import { CreateComponent } from './usuario/create/usuario-create.component';
+import { DeleteComponent } from './usuario/delete/usuario-delete.component';
+import { PlistComponent } from './usuario/plist/usuario-plist.component';
+import { ViewComponent } from './usuario/view/usuario-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    EditComponent,
+    CreateComponent,
+    DeleteComponent,
+    PlistComponent,
+    ViewComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/zona/plist/zona-plist.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PlistComponent } from './zona-plist.component';
+
+describe('PlistComponent', () => {
+  let component: PlistComponent;
+  let fixture: ComponentFixture<PlistComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PlistComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PlistComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
